3af648a782a035bc0fcd0822e0147c869a933d51
kadaiadak <teo.le.greato@gmail.com>
2025-11-15 12:16:30 -0800

    fade out

--- Scripts/Player/camera.gd
+++ Scripts/Player/camera.gd
@@ -1,9 +1,15 @@
 extends Camera2D
 
 
+#region vars
+
 @onready var ap: AnimationPlayer = $AnimationPlayer
+@onready var transition_timer: Timer = $TransitionTimer
 
 var transitioning = false
+var trans_timer_started = false
+
+#endregion
 
 
 func _process(_delta: float) -> void:
@@ -13,4 +19,11 @@ func _process(_delta: float) -> void:
 func fade() -> void:
 	if transitioning == true:
 		ap.play("fade_in")
-		transitioning = false
+		if not trans_timer_started:
+			transition_timer.start()
+			trans_timer_started = true
+
+
+func _on_transition_timer_timeout() -> void:
+	transitioning = false
+	trans_timer_started = false
