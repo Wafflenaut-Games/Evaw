8872eadf19d5a315784f5bc1a67f4ba903624f84
Ruxxtin <109229048+Ruxxtin@users.noreply.github.com>
2025-11-15 18:47:17 -0600

    AAAAGGHHHG IM NTTING ITS SO MUCH BETTER

--- Scripts/Player/vaw.gd
+++ Scripts/Player/vaw.gd
@@ -286,7 +286,7 @@ func jump() -> void:
 		
 		# Jump Cut
 		if (Input.is_action_just_released("up") or Input.is_action_just_released("jump")) and velocity.y < 0:
-			velocity.y = 0
+			velocity.y = lerp(velocity.y, 0.0, 0.4)
 
 
 func coyote_time_set() -> void:
