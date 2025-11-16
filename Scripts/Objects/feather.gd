40e28731841ea034f0c384ce03381058bf40a36e
Ruxxtin <109229048+Ruxxtin@users.noreply.github.com>
2025-11-16 13:19:28 -0600

    smoother feathers

--- Scripts/Objects/feather.gd
+++ Scripts/Objects/feather.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
-#JAMES READ THIS BASICALLY YOU GOTTA MAKE FEATHER SPEED CHANGE WITH DIFFERENT WAVE FORMS
+#JAMES READ THIS BASICALLY YOU GOTTA MAKE FEATHER SPEED CHANGE WITH DIFFERENT WAVE FORMS done
 #AND FINISH THE LEVEL DISPLAY IN THE WORLDMAP AND MAKE THE FEATHERS RESET WHEN YOU DIE
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 @export var level: int = 1
 
@@ -10,18 +11,32 @@ var starting_pos: Vector2
 
 var collecting = false
 
-var speed = randf_range(30,70)
+var start_speed = randf_range(40,50)
+var real_speed = start_speed
 
 var player
 
 
 func _ready() -> void:
+	real_speed = start_speed
 	starting_pos = global_position
+	animated_sprite_2d.frame = randi_range(0,9)
 
 
 func _process(delta: float) -> void:
+	if Global.vaw_form == "norm":
+		real_speed = start_speed
+	if Global.vaw_form == "sine":
+		real_speed = start_speed * 0.6
+	if Global.vaw_form == "lume":
+		real_speed = start_speed * 1.5
+	
 	if collecting == true:
-		global_position = global_position.move_toward(player.global_position, speed * delta)
+		if global_position.distance_to(player.global_position) < 16:
+			real_speed = lerp(real_speed,0.0,0.8)
+		
+		global_position = global_position.move_toward(player.global_position, real_speed * delta)
+
 
 func collect(body):
 	player = body
