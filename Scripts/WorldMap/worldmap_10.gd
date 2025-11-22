0f6f0a7ea97fd74ae31384b70c5906b095cf7d09
Ruxxtin <109229048+Ruxxtin@users.noreply.github.com>
2025-11-22 16:00:29 -0600

    crt aniumation works

--- Scripts/WorldMap/worldmap_10.gd
+++ Scripts/WorldMap/worldmap_10.gd
@@ -1,6 +1,13 @@
 extends Sprite2D
-@onready var apcrt = $AnimationPlayer
-@onready var apworld = $"../Worldmap11/AnimationPlayer"
+
+@onready var apcrt: AnimationPlayer = $apcrt
+@onready var apworld: AnimationPlayer = $"../Worldmap11/apworld"
+
+
+
+func _process(delta: float) -> void:
+	playanims()
+
 
 func playanims ():
 	apworld.play("zoom2")
