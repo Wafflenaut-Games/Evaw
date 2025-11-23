9dc5cde8533029543eabc5c8d9201e74c35458c7
kadaiadak <teo.le.greato@gmail.com>
2025-11-22 21:05:16 -0800

    titlescreen options menu functionality

--- Scripts/UI/title_screen.gd
+++ Scripts/UI/title_screen.gd
@@ -1,13 +1,19 @@
 extends Control
 
 
+#region vars
+
+@onready var options_ui: Control = $OptionsUI
+
+#endregion
+
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/WorldMap/world_map.tscn")
 
 
 func _on_options_pressed() -> void:
-	pass # Replace with function body.
+	options_ui.visible = true
 
 
 func _on_quit_pressed() -> void:
