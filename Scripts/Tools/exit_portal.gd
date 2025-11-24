258e8794ddc667721790bfab0b5c6ec4fe91115f
kadaiadak <teo.le.greato@gmail.com>
2025-11-24 12:20:55 -0800

    fix exit portal

--- Scripts/Tools/exit_portal.gd
+++ Scripts/Tools/exit_portal.gd
@@ -8,6 +8,8 @@ extends Area2D
 @onready var transition_timer: Timer = $TransitionTimer
 @onready var vaw: CharacterBody2D = $"../Vaw"
 
+var entered = false
+
 #endregion
 
 
@@ -20,16 +22,27 @@ func _process(_delta: float) -> void:
 	transitions.global_position = vaw.global_position
 
 
-func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player") and body.is_grounded():
+func leave() -> void:
+	if vaw.is_grounded() and entered:
 		Global.is_transitioning = true
 		transitions.play("close")
 		transition_timer.start()
-		body.inactive = true
+		vaw.inactive = true
 		Global.lvl_completed = Global.level
+	
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		entered = true
 
 
 func _on_transition_timer_timeout() -> void:
 	Global.completed_lvls.append(Global.level)
 	Global.level = 0
 	get_tree().change_scene_to_file("res://Scenes/WorldMap/world_map.tscn")
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		entered = false
