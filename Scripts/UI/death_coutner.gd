c1135b1d705661241b3065371e13f2f5868731e6
kadaiadak <teo.le.greato@gmail.com>
2025-11-25 16:25:32 -0800

    remove death counter mechanics

--- Scripts/UI/death_coutner.gd
+++ Scripts/UI/death_coutner.gd
@@ -3,23 +3,15 @@ extends Control
 
 @onready var label: Label = $PanelContainer/Label
 
-var deaths_in_tens: int
-
 
 func _process(_delta: float) -> void:
-	remove_hunds()
 	update_label()
-
-
-func remove_hunds() -> void:
-	if Global.death_count < 100:
-		deaths_in_tens = Global.death_count
-	else:
-		deaths_in_tens = Global.death_count - floor(float(Global.death_count)/100) * 100
 
 
 func update_label() -> void:
 	if Global.death_count < 10:
-		label.text = "   0" + str(deaths_in_tens)
+		label.text = "00" + str(Global.death_count)
+	elif Global.death_count < 100:
+		label.text = "0" + str(Global.death_count)
 	else:
-		label.text = "   " + str(deaths_in_tens)
+		label.text = str(Global.death_count)
