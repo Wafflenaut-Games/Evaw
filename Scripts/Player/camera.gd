a656c38b3d7baa29211a0f3696c0ecca15878591
kadaiadak <teo.le.greato@gmail.com>
2025-11-26 23:32:06 -0800

    feather counter stuff

--- Scripts/Player/camera.gd
+++ Scripts/Player/camera.gd
@@ -1,4 +1,4 @@
-extends Camera2D #whatever limits is does smt, but fade and the transition timer will be replaced by the reverse transition anim
+extends Camera2D
 
 
 #region vars
@@ -20,15 +20,7 @@ var shake_timer_started = false
 
 func _process(_delta: float) -> void:
 	fade()
-	limits()
 	screen_shake(_delta)
-	
-	if Global.level == 0:
-		$Feather.visible = true
-		$feather_counter.visible = true
-	else:
-		$Feather.visible = false
-		$feather_counter.visible = false
 
 
 func fade() -> void:
@@ -40,7 +32,8 @@ func fade() -> void:
 
 func _on_transition_timer_timeout() -> void:
 	trans_timer_started = false
-	
+
+
 func screen_shake(delta) -> void:
 	if shake == true:
 		shake_strength = randomShakeStrength
@@ -50,13 +43,6 @@ func screen_shake(delta) -> void:
 	if shake_strength > 0:
 		shake_strength = lerpf(shake_strength, 0, shakeFade * delta)
 		offset = Vector2(rng.randf_range(-shake_strength, shake_strength), rng.randf_range(-shake_strength, shake_strength))
-
-func limits():
-	pass
-#	if Global.level == 3:
-#		limit_left = -64
-#	if Global.level == 2:
-#		limit_left = -55
 
 
 func _on_shake_timer_timeout() -> void:
