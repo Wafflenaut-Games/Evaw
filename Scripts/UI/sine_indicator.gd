4dadaece49ace2356b62f450fe389dbcd8002a5a
kadaiadak <teo.le.greato@gmail.com>
2025-11-27 22:20:04 -0800

    fixed fixing that stuff

--- Scripts/UI/sine_indicator.gd
+++ Scripts/UI/sine_indicator.gd
@@ -5,8 +5,12 @@ extends AnimatedSprite2D
 
 
 func _process(_delta: float) -> void:
-	if Global.difficulty == 0:
-		if not vaw.sine_used:
-			play("unused")
-		else:
-			play("used")
+	if Global.difficulty == 4:
+		visible = true
+	else:
+		visible = false
+	
+	if not vaw.sine_used:
+		play("unused")
+	else:
+		play("used")
