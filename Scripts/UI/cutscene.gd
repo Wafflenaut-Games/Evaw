3a137aece2d362007000e94150f69be2c3db6557
Ruxxtin <jamesbglaser13@gmail.com>
2025-11-29 16:44:03 -0600

    transition stuff

--- Scripts/UI/cutscene.gd
+++ Scripts/UI/cutscene.gd
@@ -7,8 +7,20 @@ var lines: Array = ["In a world filled with echoes and light",
 
 var current_line = 0
 
+var skipping = false
+
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var text: Label = $text
+
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("skip"):
+		skipping = true
+		$Transitions.visible = true
+		$Transitions.play("close")
+	
+	if skipping == true:
+		$song.volume_db -= 0.5
 
 
 func _ready() -> void:
